aaa0d07ce0df8772fd76a72ecbeba35996c153ab
Julia Cosma <37083087+juliacosma@users.noreply.github.com>
2019-05-04 17:55:49 -0400

    Add files via upload

---
+++ FirstSonicPi.rb
@@ -0,0 +1,56 @@
+# first Sonic Pi experiment
+#This is a Satie inspired pieced I wrote on guitar originally
+
+2.times do
+  play :Gs2
+  sleep 1
+  play :B3
+  play :Ds4
+  play :G24
+  sleep 1
+  play :Ds2
+  sleep 1
+  play :B3
+  play :Ds4
+  play :G24
+  sleep 1
+end
+
+#################
+play :Gs2
+sleep 1
+play :B3
+play :Ds4
+play :G24
+sleep 1
+play :As4
+sleep 0.5
+play :A4
+sleep 0.5
+play :B3
+play :Ds4
+play :G24
+sleep 1
+play :Fs4
+sleep 0.5
+play :F4
+sleep 0.5
+play :Gs2
+play :D4
+sleep 1
+play :B3
+play :Ds4
+play :G24
+
+
+
+
+
+
+
+
+
+
+
+
+
